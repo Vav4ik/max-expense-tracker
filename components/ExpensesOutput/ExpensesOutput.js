67d1cd1ccfc6d83761eb94f2a4fabd325719e8c4
Valdis Indricans <vav4ik@pop-os.localdomain>
2023-10-12 16:22:15 +0100

    Created DUMMY_DATA and basic output

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -3,11 +3,44 @@ import { View } from "react-native";
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    description: "Pair of shoes",
+    amount: 39.99,
+    date: new Date("2023-09-22"),
+  },
+  {
+    id: "e2",
+    description: "Book",
+    amount: 19.9,
+    date: new Date("2023-10-12"),
+  },
+  {
+    id: "e3",
+    description: "Jacket",
+    amount: 89.99,
+    date: new Date("2023-10-11"),
+  },
+  {
+    id: "e4",
+    description: "Some bananas",
+    amount: 1.99,
+    date: new Date("2023-10-10"),
+  },
+  {
+    id: "e5",
+    description: "Sport equipment, dumbell",
+    amount: 399.00,
+    date: new Date("2023-08-12"),
+  },
+];
+
 const ExpensesOutput = ({ expenses, expensesPeriod }) => {
   return (
     <View>
-      <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
-      <ExpensesList />
+      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
+      <ExpensesList expenses={DUMMY_EXPENSES} />
     </View>
   );
 };
